0e2b7e722e6e282d6a025ffeeedcd5ddc2189412
thepedrodev <thepedrodev@gmail.com>
2025-08-22 08:53:17 -0300

    Alterando aquivo de validacao das variaveis de ambiente

--- src/env.ts
+++ src/env.ts
@@ -2,16 +2,9 @@ import { z } from "zod"
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string().min(1)
+  JWT_SECRET: z.string().min(1),
+  PORT: z.coerce.number().default(3333)
 })
 
-const _env = envSchema.safeParse(process.env)
+export const env = envSchema.safeParse(process.env)
 
-if (!_env.success) {
-  console.error("Variáveis de ambiente inválidas", _env.error.format())
-  throw new Error("Erro ao carregar variáveis de ambiente.")
-}
-
-const env = _env.data
-
-export { env }
